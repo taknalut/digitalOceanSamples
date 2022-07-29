ba87e40bbd00680617aa91b8da3dbea146285901
Nalut Sangtakraw <nsangtakraw@zerionsoftware.com>
2022-07-29 15:09:49 -0400

    added console log

--- packages/qr/qr/qr.js
+++ packages/qr/qr/qr.js
@@ -1,6 +1,7 @@
 const qrcode = require('qrcode')
 
 exports.main = (args) => {
+  console.log("ARGS", args)
   return qrcode.toDataURL(args.text).then(res => ({
     headers:  { 'content-type': 'text/html; charset=UTF-8' },
     body: args.img == undefined ? res : `<img src="${res}">`
